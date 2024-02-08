70389bb1aacdc1b4468a741472604610028e6f17
gabriele-sala <83841730+gabriele-sala@users.noreply.github.com>
2024-02-08 21:52:59 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DragCalculator from './DragCalculator'; // Import your component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<DragCalculator />);
