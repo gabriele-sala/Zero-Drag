4865afc17c0b0a0120d5456a9a8042118f8347c9
gabriele-sala <83841730+gabriele-sala@users.noreply.github.com>
2024-02-08 22:42:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { Container, Grid, Typography } from '@mui/material'; 
+import { useState, useEffect } from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+import DragCalculator from './components/DragCalculator'; // Assuming this is placed inside a 'components' folder in src
+import AirDensityInput from './components/AirDensityInput"; 
+// If other components like F1Image, Formula1DragInfo, DragCoefficientTable are created, import them here as well
 
-import AirDensityInput from './components/AirDensityInput';
-import DragCalculator from './components/DragCalculator';
-import DragCoefficientTable from './components/DragCoefficientTable';
-import Formula1DragInfo from './components/Formula1DragInfo';
-import F1Image from './components/F1Image';
-
+ 
 function App() {
   return (
     <Container maxWidth="lg">
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+      <Grid container spacing={3} direction="column"> 
+        <Grid item>
           <Typography variant="h3" align="center" gutterBottom>
-            F1 Aerodynamic Drag Calculator
+            F1 Aerodynamic Drag Calculator 
           </Typography>
-          <F1Image />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Formula1DragInfo />
+        <Grid item>
+          <AirDensityInput  /> 
         </Grid>
-
-        <Grid item xs={12}>
-          <AirDensityInput />
-        </Grid>
-        <Grid item xs={12}>
-          <DragCalculator /> 
-        </Grid>
-        <Grid item xs={12}>
-          <DragCoefficientTable />
-        </Grid>
+        <Grid item> 
+          <DragCalculator />  
+        </Grid> 
       </Grid>
     </Container>
   );
 }
+
 
 export default App;
