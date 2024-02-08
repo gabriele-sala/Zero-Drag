7a01fcce5f4c347fb72c6e68e3a857fa7d65efb3
gabriele-sala <83841730+gabriele-sala@users.noreply.github.com>
2024-02-08 22:42:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; 
-import './index.css'; // Add your global styles 
+import App from './App'; // import your main App component
+import './index.css"; // (create, import general styles if needed)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />); 
+root.render(<App />);
 
